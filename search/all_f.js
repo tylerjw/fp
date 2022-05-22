74199ad4d1e3133244cb7ef2569907c544287f84
tylerjw <squirrel428@protonmail.com>
2022-05-22 18:51:38 +0000

    Deploying to gh-pages from @ tylerjw/fp@d025a90ebcd331cd345369f13864a73720f0ca87 🚀

--- search/all_f.js
+++ search/all_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['unauthenticated_50',['UNAUTHENTICATED',['../namespacefp.html#ab7edc7bd4d0d366ca827a5a09b71f3cca456dc2351b6dbb9db3b40552b2e8c5a9',1,'fp::UNAUTHENTICATED()'],['../namespacefp.html#a49bf582bc32c85001b9ca42b5a0f1942',1,'fp::Unauthenticated()']]],
-  ['unavailable_51',['UNAVAILABLE',['../namespacefp.html#ab7edc7bd4d0d366ca827a5a09b71f3cca084f4ca2c0fe703e46514a144b4d6ead',1,'fp::UNAVAILABLE()'],['../namespacefp.html#a09adfeacfa1ad33ed6ee12324ec699fe',1,'fp::Unavailable()']]],
+  ['unavailable_51',['Unavailable',['../namespacefp.html#a09adfeacfa1ad33ed6ee12324ec699fe',1,'fp::Unavailable()'],['../namespacefp.html#ab7edc7bd4d0d366ca827a5a09b71f3cca084f4ca2c0fe703e46514a144b4d6ead',1,'fp::UNAVAILABLE()']]],
   ['unimplemented_52',['UNIMPLEMENTED',['../namespacefp.html#ab7edc7bd4d0d366ca827a5a09b71f3ccaaa528108d473f663f8947b4d16e7c13c',1,'fp::UNIMPLEMENTED()'],['../namespacefp.html#ae0e7256ba961aca8d5407dfce1d760c2',1,'fp::Unimplemented()']]],
-  ['unknown_53',['Unknown',['../namespacefp.html#a8e38cd143ac13ab9a09402c4dcb262c0',1,'fp::Unknown()'],['../namespacefp.html#ab7edc7bd4d0d366ca827a5a09b71f3cca696b031073e74bf2cb98e5ef201d4aa3',1,'fp::UNKNOWN()']]]
+  ['unknown_53',['UNKNOWN',['../namespacefp.html#ab7edc7bd4d0d366ca827a5a09b71f3cca696b031073e74bf2cb98e5ef201d4aa3',1,'fp::UNKNOWN()'],['../namespacefp.html#a8e38cd143ac13ab9a09402c4dcb262c0',1,'fp::Unknown()']]]
 ];
